34bc4c1377d815272b46373e33a2ec48ac16d9c8
Shivangi Bharani <shivangibharani@dhcp-128-189-150-79.ubcsecure.wireless.ubc.ca>
2024-10-03 21:11:15 -0700

    added supabase

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ from openai import OpenAI
 import json
 import os
 from urllib.parse import urlparse
+import difflib
+from supabase import create_client, Client
 from dotenv import load_dotenv
 
 load_dotenv()
 
 OPEN_AI_KEY = os.getenv('OPEN_AI_KEY')
+SUPABASE_URL = os.getenv('SUPABASE_URL')
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 client = OpenAI(
     api_key = OPEN_AI_KEY
@@ -28,8 +34,6 @@ allowed_origins = ["*"]
 app.add_middleware(CORSMiddleware, allow_origins=allowed_origins,
                    allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-messages = []
-
 @app.get("/")
 def read_root():
     return {"message": "Working"}
@@ -43,39 +47,41 @@ def tinify(command: CommandRequest):
     github_files = get_github_files(repo_url)
 
     # Get the file that is most important
-    optional_text = f'Which 1 of these files do you think would be most important to solve this issue: {json.dumps(github_files)} Return as a json with "file_name" as the key and should have only 1 value. Do not give as a code snippet.'
-    required_files = send_openAI_request(prompt, optional_text)
-    messages.append({"role": "user", "content": f'{prompt}. {optional_text}'})
-    messages.append({"role": "assistant", "content": f'{required_files}'})
+    required_files = send_openAI_request(prompt, optional_text=f'Which 1 of these files do you think would be most important to solve this issue (usually would be a python file): {json.dumps(github_files)} Return as a json with "file_name" as the key and should have only 1 value that should be the name from the given array. Do not give as a code snippet.')
 
     # Get the username and repo name from the url to create a raw url
     user , repo = get_user_repo_from_github_url(repo_url)
 
     # Store the file name
     file_name = json.loads(required_files)["file_name"]
+    print(file_name)
 
     # Get file content from github
     file_content = fetch_file_content(file_name, user, repo)
 
     # Get code improvements
-    optional_text = f'Modify this file (only if it solves the problem) to solve the problem. Only return the new code snippet (if nothing should be changed, return the original file content): {file_content}'
-    gpt_suggestion = send_openAI_request(prompt, optional_text)
-    messages.append({"role": "user", "content": f'{prompt}. {optional_text}'})
-    messages.append({"role": "assistant", "content": f'{gpt_suggestion}'})
+    gpt_suggestion = send_openAI_request(prompt, optional_text=f'Modify this file (only if it solves the problem) to solve the problem. Only return the new code snippet (if nothing should be changed, return the original file content): {file_content}')
     
     # Ask if it wants to correct it's answer
     want_to_reflect = send_openAI_request(prompt="Are you sure about this or do you want to correct your answer? (answer with a 1 or 0 and not include a full stop.)")
-    messages.append({"role": "user", "content": "Are you sure about this or do you want to correct your answer? (answer with a 1 or 0 and not include a full stop.)"})
-    messages.append({"role": "assistant", "content": f'{want_to_reflect}'})
 
     if int(want_to_reflect):
-        gpt_suggestion = send_openAI_request(prompt, optional_text=f'Correct Yourself.')
-        print(gpt_suggestion)
+        gpt_suggestion = send_openAI_request(prompt, optional_text=f'Modify this file (only if it solves the problem) to solve the problem. Only return the new code snippet (if nothing should be changed, return the original file content): {file_content}')
+    
+    gpt_suggestion = gpt_suggestion[9:-3]
+    
+    diff = ''.join(difflib.unified_diff(file_content.splitlines(keepends=True), gpt_suggestion.splitlines(keepends=True), lineterm=''))
+    # response = {
+    #     "diff": diff.replace('\n', '\\n').replace('\r', '\\r')
+    #     }
 
-    return {
-        repo_url,
-        prompt
-    }
+    response = (
+    supabase.table("tinygen")
+    .insert({"repo_url": repo_url, "prompt": prompt, "diff": diff})
+    .execute()
+    )
+
+    return diff
 
 def get_user_repo_from_github_url(github_url):
     # Parse the GitHub URL
@@ -109,7 +115,10 @@ def send_openAI_request(prompt="", optional_text=""):
     # print(optional_text)
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=messages
+        messages=[
+            {"role": "user", "content": f'{prompt}. {optional_text}'},
+        ],
+        store=True
     )
 
     return completion.choices[0].message.content
